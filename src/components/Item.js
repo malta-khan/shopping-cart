430f471d6615c3072f51b22d686644483bf00eaa
Safdar <71539665+malta-khan@users.noreply.github.com>
2023-02-01 11:03:13 +0500

    worked on styles for shop page

--- src/components/Item.js
+++ src/components/Item.js
@@ -15,9 +15,16 @@ const Item = (props)=>{
     } else{
         itemDetails =  <div>
         <ImageLoader name = {storeItem.id}></ImageLoader>
-        <div>{storeItem.title}</div>
-        <div>{storeItem.details}</div>
-        <div>{storeItem.price}</div>
+        <div className="flex flex-col">
+            <h2 className=" text-xl font-semibold text-yellow-700">{storeItem.title}</h2>
+            <div className="flex items-center">
+                <div className="w-6 h-6">
+                <ImageLoader  name = {"emerald"} ></ImageLoader> 
+                </div>
+                <div className="text-lg font-semibold font-mono text-neutral-800">{storeItem.price} <span className="text-neutral-500">/per item</span></div>
+            </div>
+        </div>
+        {/* <div>{storeItem.details}</div> */}
     </div>
     }
     
@@ -25,9 +32,11 @@ const Item = (props)=>{
     let addToCart = props.addToCart;
     const [itemQty, setItemQty] = useState(1);
     if(storeItem !== undefined){
-        cartInputs =<div>
-        <input type= "number" min="1" value={itemQty} onChange = {(e)=>{setItemQty(e.target.value)}}></input>
-        <button onClick={()=>{addToCart(storeItem.id, itemQty)}}>Add</button>
+        cartInputs =<div className="flex">
+        <label className="font-semibold">Qty:
+        <input className="w-20 p-2 ml-3 border rounded-md border-neutral-800 focus:outline-green-600" type= "number" min="1" value={itemQty} onChange = {(e)=>{setItemQty(e.target.value)}}></input>
+        </label>
+        <button className="bg-yellow-700 text-neutral-100 shadow-sm p-2 rounded-md ml-5" onClick={()=>{addToCart(storeItem.id, itemQty)}}>Add to cart</button>
         </div>
     }
 
@@ -37,6 +46,6 @@ const Item = (props)=>{
         alreadyInCart = <div>You have {qtyInCart} of this item in your cart.</div>
     }
 
-    return <div>{itemDetails} {cartInputs} {alreadyInCart}</div>
+    return <div className="max-w-xl w-full px-4">{itemDetails} {cartInputs} {alreadyInCart}</div>
 }
 export default Item
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -5,35 +5,33 @@ const Shop = (props)=>{
     //Titles of items should be clickable, which takes user to item page
     let items = props.storeItems.map((item)=>{
         return (
-        <a key={item.id} href={"/shop/" + item.id} className="flex border p-4 bg-neutral-300 focus:ring">
+        <a key={item.id} href={"/shop/" + item.id} className="flex border p-4 h-36 mb-2 rounded-md shadow-md bg-neutral-100 hover:bg-slate-200 focus:ring">
             
-            <div className=" border w-1/4 mr-4 bg-neutral-500">
-                {/* <ImageLoader className = "" name = {item.id} ></ImageLoader> */}
+            <div className="w-1/4 mr-6">
+                <ImageLoader className = "" name = {item.id} ></ImageLoader>
             </div>
 
             <div className="w-3/4">
 
                 <div className="flex justify-between">
-                    <h2>
+                    <h2 className=" text-xl font-semibold text-yellow-700">
                         {item.title}
                     </h2>
-                    <div className="flex items-end">
+                    <div className="flex items-center">
                         <div className="w-6 h-6">
                         <ImageLoader  name = {"emerald"} ></ImageLoader> 
                         </div>
-                        <div> 
-                        {item.price}
-                        </div>
+                        <div className="text-lg font-semibold font-mono text-neutral-800">x {item.price}</div>
                     </div>
                 </div>
-
-                <div>{item.details.slice(0,120)+"..."}</div> 
+                <div className="text-neutral-700">{item.details.slice(0,120)+"..."}</div> 
             </div>
         </a>
             )
     })
-    return <div className="max-w-lg space-y-2">
-        <h1>Shop Page</h1>
+    return <div className="max-w-xl w-full px-4">
+        <h1 className="text-3xl font-semibold text-yellow-700 text-center mt-10">Shop</h1>
+        <h2 className="text-2xl text-neutral-600 mt-1 text-center mb-10">A collection of best selling minecraft mobs</h2>
         {items}
         </div>
 } 
